076472bf40188fca12b2aaddcd673c1cfd59dc60
thesmartypants <alex@shmulevich.com>
2023-07-25 15:41:09 -0500

    Made the site slide in from the left for 1 second

--- previous_response.py
+++ previous_response.py
@@ -1,10 +1,2 @@
-
-class Student:
-   def __init__(self, name):
-     self.name = name
-
-   def display_name(self):
-     print(f'My name is {self.name}.')
-
-student1 = Student('Alice')
-student1.display_name()
+result = 2 + 2
+print(result)
