bcc0ce9aec5e017d6af3e98c1919db1853ed2932
thesmartypants <alex@shmulevich.com>
2023-07-25 17:54:51 -0500

    I tried doing something and this project got deleted, I just put it back, sorry.

--- previous_response.py
+++ previous_response.py
@@ -1,1 +1,11 @@
-print(2 + 2)
+
+class Car:
+   def __init__(self):
+     self.brand = 'Toyota'
+     self.model = 'Camry'
+
+   def display_car_info(self):
+     print(f'Brand: {self.brand}, Model: {self.model}')
+
+car1 = Car()
+car1.display_car_info()
